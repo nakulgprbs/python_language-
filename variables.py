1724e8ddd828399f58e55ea4d6e6313251864920
Nakul <139488807+nakulgprbs@users.noreply.github.com>
2024-01-07 10:32:39 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+# variables 
+name = "Nakul singh"
+print(name)  # name is variable name 
+
+phone_no = 985412 # phone_no is also a variable name 
+print(phone_no)
+
+# types of these variale wether it is integer or string, it may other also like bool char,etc you can check for that 
+
+print(type(name))
+print(type(phone_no))
+
+# concatinate of string can be done using + symbol 
+
+print("My name is "+ name + " and phone no. is", phone_no ) # here my name is another string concatinating with name 
