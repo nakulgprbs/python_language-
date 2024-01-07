ac53033b4471e698ee0228be10cfd3e94fd43aed
Nakul <139488807+nakulgprbs@users.noreply.github.com>
2024-01-07 11:11:33 +0530

    Add files via upload

---
+++ volume_of_sphare.py
@@ -0,0 +1,6 @@
+# volume of sphare 
+radius = float(input("enter radius: ")) 
+pi =3.14
+volume = (4/3)*pi * (radius ** 3) # formula of volume of sphare 
+# printing output
+print( "volume of sphare is: ",volume)
