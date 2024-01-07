1db7c7f09d9c4adf802c00b4c3aa476863e8c647
Nakul <139488807+nakulgprbs@users.noreply.github.com>
2024-01-08 00:07:19 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,9 @@
+# for loop with simple example 
+
+for i in range(1, 11):  # range(starting point, ending point, step)
+    print(i, "Nakul singh")
+    
+j=1  
+while j < 11:
+    print(j, "you are welcome here. ")
+    j+=1
