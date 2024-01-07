f86a59434b8d92cf4afcf505eb6e137ad751c21b
Nakul <139488807+nakulgprbs@users.noreply.github.com>
2024-01-07 11:02:33 +0530

    Add files via upload

---
+++ Operatiors.py
@@ -0,0 +1,30 @@
+# input - output function and Operatiors 
+#name = input('enter your name: ')
+# your_age = input("enter your age") # check it data type you will get string 
+#age = int(input("enter your age"))
+#print(name, age )
+ 
+ 
+ 
+# Arithmetic operator 
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
+sum = num1 + num2  # here + is addition arithmetic operator 
+dif = num1-num2   # here - is substraction operator
+product = num1*num2 # * multiplication 
+division = num1 /num2
+floor_division = num1//num2
+remainder = num1%num2
+exponentiation = num1 ** num2 
+print("Sum of numbers is ", sum)
+print("substraction of numbers is ", dif)
+print("Multiplication of numbers is ", product)
+print("Division of numbers is ", division)
+print("Floor division of numbers is ", floor_division)
+print("reminder is ", remainder)
+print("Exponentiation is ", exponentiation)
+
+
+
+
+
---
+++ ascii.py
@@ -0,0 +1,8 @@
+# ascii values   * ord can be used to get ascii value of sting of length 1
+
+char = "a"
+print(ord(char))
+
+# chr() it is opposite to ord()
+ascii =67
+print(chr(ascii))
